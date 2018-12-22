f8e7ceeab70aaf3dbfd79f96c8c49b03fc887eed
BetoRincon <aluisrincon@unisangil.edu.co>
2018-12-22 12:35:23 -0500

    delete comment

--- app/lib/firebase_configuration.js
+++ app/lib/firebase_configuration.js
@@ -1,14 +1,7 @@
 const FirebaseCore = require('firebase.core');
 const Secrets = require('/secrets').firebaseConfig;
 
-// const configObject = {
-//     APIKey: "AIzaSyCPp5TS9ckTTbnJwowL9wWmiMCYm1QA8-w",
-//     googleAppID: 'aesthetic-guild-226115',
-//     GCMSenderID: '749426280330',
-//     projectID: "test-b6ca1",
-//     storageBucket: "test-b6ca1.appspot.com",
-//     applicationID: "1:8903486192:android:c6fb5b9095fccdb8"
-// }
+
 
 const configObject = Secrets;
 
